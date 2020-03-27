e3fd9cc570d8b090fb21dc1a11c0eeed5347c83b
K4Jo73 <k4jo73@gmail.com>
2020-03-27 21:59:57 +0000

    new search function and file exist check

--- mod/FileHandler.py
+++ mod/FileHandler.py
@@ -12,23 +12,71 @@ def ShowFile(filePath="LICENSE"):
 
 # Function - Return the contents of a file
 def LoadFile(filePath="LICENSE"):
-    #Open and read the file
-    objfile = open(filePath, "r+")
-    text_in_file = objfile.read()
-    return text_in_file
+    if checkFile(filePath):
+        objfile = open(filePath, "r+")
+        text_in_file = objfile.read()
+        return text_in_file
+    return "File Not Found - Unable to load file: " + filePath
 
 
 
-# Function - Create an populate a file
+# Function - Create and populate a file
 def CreateFile(filePath,fileContent):
     objfile = open(filePath, "wb")
-    print(objfile.mode)
-    print(objfile.name)
+    print("File Mode: " + objfile.mode)
+    print("File Name: " + objfile.name)
     objfile.write(bytes(fileContent, 'UTF-8'))
     objfile.close()
+
+
+
+# Function - Append to a file
+def AppendFile(filePath,fileContent):
+    if checkFile(filePath):
+        objfile = open(filePath, "ab")
+        print("File Mode: " + objfile.mode)
+        print("File Name: " + objfile.name)
+        objfile.write(bytes(fileContent, 'UTF-8'))
+        objfile.close()
+    return "File Not Found - Unable to append file: " + filePath
 
 
 
 #Delete the file
 def DeleteFile(filePath):
     os.remove(filePath)
+
+
+
+# Function - Search the contents of a file
+def SearchFile(filePath="LICENSE",searchFor=""):
+    if checkFile(filePath):
+        objfile = open(filePath, "r")
+        lineNo = 0
+        searchResults = "Searched for [" + searchFor + "] in file [" + filePath + "]\n\nSearch Results:\n" 
+        fullFile = ""
+        fileContent = objfile.readlines()
+        for lineContent in fileContent:
+            lineNo = lineNo + 1
+            modifiedLine = formatLineWithNo(lineNo,lineContent)
+            fullFile += modifiedLine
+            if searchFor in lineContent: searchResults += modifiedLine
+        searchResults += "\n\nFull File:\n" + fullFile
+        return searchResults
+    return "File Not Found - Unable to search file: " + filePath
+
+
+
+def formatLineWithNo(LineNo=0,LineText=""):
+    displayLineNo = "    " + str(LineNo)
+    return "[" + displayLineNo[-4:] + "] " + LineText
+
+
+
+def checkFile(filePath):
+    try:
+        with open(filePath) as f:
+            return 1
+    except IOError:
+        return 0
+
--- wip.py
+++ wip.py
@@ -1,3 +1,4 @@
+
 import random
 import sys
 
@@ -5,18 +6,29 @@ sys.path.append('./mod/')
 #print(sys.path)
 import FileHandler
 
-print('\n' * 2)
+print('_' * 50)
+print('')
 print("Testing Custom Module Design")
-print('\n' * 2)
+#print('\n' * 2)
+print('_' * 50)
 
-FileHandler.ShowFile('README.md')
+#FileHandler.ShowFile('README.md')
 
-strFileContent = FileHandler.LoadFile()
-print(strFileContent)
+#strFileContent = FileHandler.LoadFile()
+#print(strFileContent)
 
 #FileHandler.CreateFile("File1.txt","My sample text\nand some more text")
+#FileHandler.AppendFile("File1.txt","\nThis is new text appended to the file\nand some more text")
 
-FileHandler.DeleteFile("File1.txt")
+
+#MySearchResults = FileHandler.SearchFile("LICENSE","public")
+#print(MySearchResults)
+
+MySearchResults = FileHandler.SearchFile("File1.txt","and")
+print(MySearchResults)
+
+
+#FileHandler.DeleteFile("File1.txt")
 
 
 #append to file
